24bda9f981af4bc017379a97c7f0b2f889add58e
jimmoyle <git@jimmoyle.com>
2017-05-30 06:16:09 +0100

    JM + Added comments in code

--- Recursive.ps1
+++ Recursive.ps1
@@ -2,10 +2,10 @@
 function Set-Text {
     <#
     .SYNOPSIS
-    Walks an object with recursion
+    Walks an object with recursion.
 
     .DESCRIPTION
-    If the item in the objec has children the item is a heading and the depth into the object is recorded.  If the item is at the end of the branch and has no children, that item is a table entry.
+    If the item in the object has children the item is a heading and the depth into the object is recorded.  If the item is at the end of the branch and has no children, that item is a table entry.
 
     .PARAMETER Data
     The initial object you want the function to walk
@@ -18,9 +18,23 @@ function Set-Text {
 
     This will walk the $PVSData object and output according to Heading and Table
 
+    Line    Property                      Value                                    Depth
+    ----    --------                      -----                                    -----
+    Table   Version                       7.13.0.13008                                 2
+    Table   FarmName                      MyFarm                                       3
+    Table   Description                                                                3
+    Heading General                                                                    2
+    Table   AuthGroupName                 JimMoyle.local/Builtin/Administrators        3
+    Table   AuthGroupName                 JimMoyle.local/Users/Domain Admins           3
+    Heading Security                                                                   2
+    Table   AuthGroupName                 JimMoyle.local/Builtin/Administrators        3
+    Table   AuthGroupName                 JimMoyle.local/Users/Domain Admins           3
+    Table   AuthGroupName                 JimMoyle.local/Users/PVSLondonSiteAdmins     3
+
     .NOTES
     General notes
     #>
+
     [CmdletBinding()]
     param (
 
@@ -46,40 +60,41 @@ function Set-Text {
 
     PROCESS {
 
-        #$properties = $Data | get-member -type NoteProperty | Select-Object -ExpandProperty Name
+        #Get object properties so that we can pass them all through the recursion
         $properties = $Data.psobject.Properties | Select-Object -ExpandProperty Name
 
         foreach ($property in $properties) {
             try {
-
+                #If the data is Null or a Custom Object or an Array, then make it a heading else default to table entry.
+                #Both custom objects and Arrays should be created by the script.
                 switch ($true) {
                     ($null -eq $Data.$property) {
                         #Write-Output "Null Table Entry $property $Depth"
                         $output = [PSCustomObject]@{
-                            Line = 'Table'
+                            Line     = 'Table'
                             Property = $property.ToString()
-                            Value = $null
-                            Depth = $Depth
+                            Value    = $null
+                            Depth    = $Depth
                         }
                         break
                     }
                     ($Data.$property.GetType().Name -ne 'PSCustomObject' -and $Data.$property.GetType().BaseType.ToString() -ne 'System.Array') {
                         #Write-Output "Match Table Entry $($property.ToString())"
                         $output = [PSCustomObject]@{
-                            Line = 'Table'
+                            Line     = 'Table'
                             Property = $property.ToString()
-                            Value = $Data.$property.ToString()
-                            Depth = $Depth
+                            Value    = $Data.$property.ToString()
+                            Depth    = $Depth
                         }
                         break
                     }
                     Default {
                         #Write-Output "Default Heading Entry $property $Depth"
                         $output = [PSCustomObject]@{
-                            Line = 'Heading'
+                            Line     = 'Heading'
                             Property = $property.ToString()
-                            Value = $null
-                            Depth = $Depth
+                            Value    = $null
+                            Depth    = $Depth
                         }
                         $Data.$property | Set-Text -Depth ($Depth + 1)
                     }
@@ -88,15 +103,12 @@ function Set-Text {
             catch {
                 Write-Host 'bug'
             }
+            #Output is from loop rather than gathering up so that if data is pipelined out of the function it can be worked on quicker
             Write-Output $output
         }
     }
-
-
     END {
-
     }
-
 }
 
 $PVSdata = Get-Content (Join-Path $PSScriptRoot pvs.json) | ConvertFrom-Json
